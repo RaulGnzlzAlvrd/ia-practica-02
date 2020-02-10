70042a4d89d69ff21d4dcc4c327bb9073e528812
Raúl González <gonzalv@ciencias.unam.mx>
2020-02-09 23:44:08 -0600

    [FINISHED] Terminado y listo para enviar (sin punto extra)

--- gatos/Gatos.java
+++ gatos/Gatos.java
@@ -199,7 +199,12 @@ public class Gatos extends PApplet {
             Gato actual = listaAbierta.remove();
             LinkedList<Gato> sucesores = actual.generaSucesores();
             if (sucesores != null) {
-                listaAbierta.addAll(sucesores);
+                for (int j = 0; j < sucesores.size(); j++) {
+                    Gato sucesor = sucesores.get(j);
+                    if (!listaAbierta.contains(sucesor)) {
+                        listaAbierta.add(sucesor);
+                    }
+                }
             }
         }
 
